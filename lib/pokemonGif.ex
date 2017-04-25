8112ebe01acd9784cea446998212fecd4aa20794
Miguel <diazmiiguel@gmail.com>
2017-04-25 15:10:39 -0400

    Some pokemon with special characters in name were not being recognized

--- lib/pokemonGif.ex
+++ lib/pokemonGif.ex
@@ -6,22 +6,32 @@ defmodule PokemonGif do
 
   def get_gif(name) when is_integer(name) do
     poke_name = Enum.at(@pokemon, name - 1)
-    poke_name
-    |> String.replace(~r/\./, "")
-    |> String.replace(~r/'/, "")
-    |> String.replace(~r/\s/, "-")
-    "#{@base_url}#{String.downcase(poke_name)}.gif"
+    "#{@base_url}#{poke_name |> clean_name() |> String.downcase()}.gif"
   end
 
   def get_gif(name) when is_bitstring(name) do
-    if member?(@pokemon, String.capitalize(name)) do
-      "#{@base_url}#{String.downcase(name)}.gif"
+    if member?(organize_list(@pokemon), clean_name(name)) do
+      "#{@base_url}#{name |> clean_name() |> String.downcase()}.gif"
     else
       "Pokemon identifier must be an integer or a valid Pokemon name as string"
     end
   end
 
+  defp organize_list(list) do
+    Enum.map(list, fn names -> clean_name(names) end)
+  end
+
   defp member?(list, item_checker) do
-    list |> Enum.map(&(&1)) |> Enum.member?(item_checker)
+    list 
+    |> Enum.map(&(&1)) 
+    |> Enum.member?(item_checker)
+  end
+
+  defp clean_name(name) do
+    name
+    |> String.replace(~r/\./, "")
+    |> String.replace(~r/'/, "")
+    |> String.replace(~r/\s/, "-")
+    |> String.downcase()
   end
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PokemonGif.Mixfile do
 
   def project do
     [app: :pokemon_gif,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -18,11 +18,12 @@ defmodule PokemonGif.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :credo]]
   end
 
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :dev}]
+    [{:ex_doc, ">= 0.0.0", only: :dev},
+     {:credo, "~> 0.7.3"}]
   end
 
   defp description do
